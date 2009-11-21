aa2da0e011c33e57b7ff994a2ad9aaf0e9efca89
Marty Smyth <martian@midgard.org>
2009-11-20 17:54:07 -0800

    First should-be-working version

--- lib/linkscape.rb
+++ lib/linkscape.rb
@@ -10,20 +10,24 @@ module Linkscape
   class Client
     def initialize(*args)
       options = Hash === args.last ? args.pop : {}
-      @accessID = args.empty? ? args.shift : (options[:id] || options[:ID] || options[:accessID])
-      @secretKey = args.empty? ? args.shift : (options[:secret] || options[:secretKey] || options[:key])
+      @accessID = args.first ? args.shift : (options[:id] || options[:ID] || options[:accessID])
+      @secretKey = args.first ? args.shift : (options[:secret] || options[:secretKey] || options[:key])
       
       @options = {
         :apiHost => 'lsapi.seomoz.com',
-        :apiRoot => 'linkscape'
+        :apiRoot => 'linkscape',
+        :accessID => @accessID,
+        :secretKey => @secretKey
       }.merge(options)
       
     end
 
     def mozRank(*args)
       options = Hash === args.last ? args.pop : {}
-      url = args.empty? ? args.shift : options[:url]
-      
+      url = args.first ? args.shift : options[:url]
+      puts @options.inspect
+      puts options.inspect
+      puts url.inspect
       Linkscape::Request.new(@options.merge(options).merge({:api => 'mozrank', :url => url})).run
     end
     
@@ -37,45 +41,66 @@ module Linkscape
   class Request
     require 'net/http'
     require 'uri'
+    require 'base64'
+    require 'cgi'
+    require 'rubygems'
+    require 'hmac-sha1'
     
-    URL_TEMPLATE = %Q[http://:apiHost:/:apiRoot:/:api:/:url:?AccessID=:accessID:&Expires=:expires:&Signature=:signature:]
+    URL_TEMPLATE = %Q[http://:apiHost:/:apiRoot:/:api:/:url:?AccessID=:accessID:&Expires=:expiration:&Signature=:signature:]
     
     def initialize(options)
-      url = URI.escape(options[:url].sub(/^https?:\/\//, ''))
-      @requestURL = URL_TEMPLATE.template(options.merge(
-                      :url => url
-                    ))
+      puts options.inspect
+      url = CGI::escape(options[:url].sub(/^https?:\/\//, ''))
+      @requestURL = URL_TEMPLATE.template(signRequest(options.merge(:url => url)))
     end
     
     def run
+      # return @requestURL
       res = fetch(URI.parse(@requestURL))
       return Response.new(self, res)
-    end
-
-    def signRequest
     end
 
     class RecursionError < StandardError; end
     class AuthenticationError < StandardError; end
     
     private
+    def signRequest options
+      id = options[:accessID]#CGI::escape(options[:accessID])
+      expiration = Time.now.to_i + 600
+      key = options[:secretKey]#CGI::escape(options[:secretKey])
+      signature = CGI::escape(
+                    Base64.encode64(
+                      HMAC::SHA1.digest(
+                        key,
+                        "#{id}\n#{expiration}"
+                      )
+                    ).chomp
+                  )
+      options[:expiration] = expiration
+      options[:signature] = signature
+      puts options.inspect
+      options
+    end
+
     def fetch(uri, limit = 10)
       # You should choose better exception.
       raise RecursionError, 'HTTP redirect too deep' if limit == 0
 
       response = Net::HTTP.get_response(uri)
-      case response
-      when Net::HTTPSuccess     then response
-      when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-      else
-        response.error!
-      end
+
+      return fetch(response['location'], limit - 1) if Net::HTTPSuccess == response
+
+      response
     end
 
   end
   
   class Response
+    attr_accessor :request, :response
+
     def initialize(request, response)
+      @request = request
+      @response = response
     end
   end
   
