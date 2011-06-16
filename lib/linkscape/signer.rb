6c6cae21f2700a1466c503ae5040a63ff24e07a1
Brad Seefeld <brad@urbaninfluence.com>
2011-06-16 15:01:47 -0700

    Making better use of configuration data that we have already been given

--- lib/linkscape/signer.rb
+++ lib/linkscape/signer.rb
@@ -5,8 +5,9 @@ require 'hmac-sha1'
 module Linkscape
   class Signer
     
-    def self.sign_params(params, keys_to_sign = [:access_id, :expiration], key = nil)
+    def self.sign_params(params, keys_to_sign = [:access_id, :expiration])
       params[:expiration] ||= Time.now.to_i + 60
+      params[:access_id] ||= Linkscape.config.access_id
       string_to_sign = keys_to_sign.collect{|k| params[k].to_s}.join("\n")
       key ||= Linkscape.config.access_key
       signature = CGI::escape(Base64.encode64(HMAC::SHA1.digest(key, string_to_sign)).chomp)
