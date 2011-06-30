3e0af953558a436e199e84024e36e832e27dda6f
Brad Seefeld <brad@urbaninfluence.com>
2011-06-30 13:53:10 -0700

    Making site a config value

--- lib/linkscape.rb
+++ lib/linkscape.rb
@@ -10,16 +10,17 @@ require 'linkscape/signer'
 
 
 module Linkscape
-  
+
   def self.config
     @config ||= Configuration.new
   end
-  
+
   def self.configure
     yield config
-    
-    Linkscape::Resource.user = config.access_id
+
+    Linkscape::Resource.user     = config.access_id
     Linkscape::Resource.password = config.access_key
-    Linkscape::Resource.timeout = config.timeout
+    Linkscape::Resource.timeout  = config.timeout
+    Linkscape::Resource.site     = config.site
   end
 end
--- lib/linkscape/configuration.rb
+++ lib/linkscape/configuration.rb
@@ -7,13 +7,16 @@ require "linkscape/session_cache"
 # @author Brad Seefeld (brad@urbaninfluence.com)
 module Linkscape
   class Configuration
-    attr_accessor :cache, :logger, :access_id, :access_key, :cache_for, :timeout
+    attr_accessor :cache, :logger, :access_id, :access_key, :cache_for, :timeout, :site
     
     def initialize
       self.cache = Linkscape::SessionCache.new
       self.cache_for = 60
       self.logger = Logger.new(STDOUT)
       self.timeout = 60
+      
+      # URL to API.
+      self.site = "http://lsapi.seomoz.com/linkscape"
     end
   end
 end
