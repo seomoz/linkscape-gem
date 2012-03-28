ef011370061d8c5f65c1854aaf49549abf9bf1c9
Evan Battaglia <evan@seomoz.org>
2012-03-28 14:37:12 -0700

    Set default expiration to 5 minutes.
    This should clear some errors that could be due to clock drift on API
    servers.

--- lib/linkscape/signer.rb
+++ lib/linkscape/signer.rb
@@ -2,7 +2,7 @@ module Linkscape
   class Signer
     
     def self.signParams params, keysToSign=[:accessID, :expiration], key = nil
-      params[:expiration] ||= Time.now.to_i + 60
+      params[:expiration] ||= Time.now.to_i + 300
       stringToSign = keysToSign.collect{|k| params[k].to_s}.join("\n")
       key ||= params[:secretKey]
       signature = CGI::escape( Base64.encode64( HMAC::SHA1.digest( key, stringToSign ) ).chomp )
