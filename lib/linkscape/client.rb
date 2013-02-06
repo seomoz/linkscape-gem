127588491336fd6347208383d567b408b49cca92
Brandon Renfrow <brenfrow@seomoz.com>
2013-02-06 10:44:45 -0800

    putting initialize under test

--- lib/linkscape/client.rb
+++ lib/linkscape/client.rb
@@ -1,17 +1,19 @@
 module Linkscape
   class Client
+
+    attr_reader :options
+
     def initialize(*args)
       options = Hash === args.last ? args.pop : {}
       @accessID = args.first ? args.shift : (options[:id] || options[:ID] || options[:accessID])
       @secretKey = args.first ? args.shift : (options[:secret] || options[:secretKey] || options[:key])
-      
+
       @options = {
         :apiHost => 'lsapi.seomoz.com',
         :apiRoot => 'linkscape',
         :accessID => @accessID,
         :secretKey => @secretKey
       }.merge(options)
-      
     end
 
 
@@ -40,9 +42,9 @@ module Linkscape
       options[:query] = {
         'Cols' => translateBitfield(options[:cols], options[:columns], options[:fields], :type => :url)
       }
-      
+
       raise MissingArgument, "urlMetrics requires a list of columns to return." unless options[:query]['Cols'].nonzero?
-      
+
       Linkscape::Request.run(@options.merge(options))
     end
 
@@ -51,9 +53,9 @@ module Linkscape
       options = Hash === args.last ? args.pop.symbolize_keys : {}
       url = args.first ? args.shift : options[:url]
       whichSet = (args.first ? args.shift : (options[:set] || 'none')).to_sym
-      
+
       raise MissingArgument, "topLinks requires a set (:page, :subdomain, :domain) and a url." unless whichSet and url
-      
+
       options[:url] = url
 
       options[:api] = {
@@ -61,9 +63,9 @@ module Linkscape
         :subdomain => 'subdomain-linklist',
         :domain => 'rootdomain-linklist',
       }[whichSet]
-      
+
       raise InvalidArgument, "topLinks set must be one of :page, :subdomain, :domain" unless options[:api]
-      
+
       options[:query] = {
         'SourceCols' => translateBitfield(options[:sourcecols], options[:sourcecolumns], options[:urlcols], :type => :url),
         'TargetCols' => translateBitfield(options[:targetcols], options[:targetcolumns], options[:urlcols], :type => :url),
@@ -71,7 +73,7 @@ module Linkscape
       }
 
       raise MissingArgument, "topLinks requires a list of columns for Source, Target, and/or Link." unless options[:query]['SourceCols'].nonzero? or options[:query]['TargetCols'].nonzero? or options[:query]['LinkCols'].nonzero?
-      
+
       Linkscape::Request.run(@options.merge(options))
     end
 
@@ -113,24 +115,24 @@ module Linkscape
       }
 
       raise MissingArgument, "allLinks requires a list of columns for Source, Target, and/or Link." unless options[:query]['SourceCols'].nonzero? or options[:query]['TargetCols'].nonzero? or options[:query]['LinkCols'].nonzero?
-      
+
       Linkscape::Request.run(@options.merge(options))
     end
 
-    
+
     def topPages(*args)
       options = Hash === args.last ? args.pop.symbolize_keys : {}
       url = args.first ? args.shift : options[:url]
-      
+
       options[:url] = url
       options[:api] = 'top-pages'
-      
+
       options[:query] = {
         'Cols' => translateBitfield(options[:cols], options[:columns], options[:sourcecols], options[:sourcecolumns], options[:urlcols], :type => :url),
       }
 
       raise MissingArgument, "topPages requires a list of columns to return." unless options[:query]['Cols'].nonzero?
-      
+
       Linkscape::Request.run(@options.merge(options))
     end
 
@@ -178,7 +180,7 @@ module Linkscape
       }
 
       # raise MissingArgument, "anchorMetrics requires a list of columns to return." unless options[:query]['Cols'].nonzero?
-      
+
       Linkscape::Request.run(@options.merge(options))
     end
 
@@ -190,9 +192,9 @@ module Linkscape
     def inspect
       %Q[#<#{self.class}:#{"0x%x" % self.object_id} api="#{@options[:apiHost]}/#{@options[:apiRoot]}" accessID="#{@options[:accessID]}">]
     end
-    
+
     private
-    
+
     def translateBitfield *columns
       options = Hash === columns.last ? columns.pop : {}
 
@@ -200,7 +202,7 @@ module Linkscape
       columns.compact!
 
       bits = columns.inject(0) do |bitfield, key|
-        requestBit = 
+        requestBit =
           if options[:type] == :url
             Linkscape::Constants::URLMetrics::RequestBits[key]
           elsif options[:type] == :link
@@ -213,9 +215,9 @@ module Linkscape
         raise InvalidArgument, "Unknown #{options[:type]} flag '#{key.inspect}'".gsub(/  +/, ' ') unless requestBit
         bitfield |= requestBit[:flag]
       end
-      
+
       bits
     end
-    
+
   end
 end
