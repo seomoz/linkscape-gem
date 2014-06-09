b89d03dd8f319eb95c091373db01b50e9978eabb
koos kleven <koos42@gmail.com>
2014-06-09 16:27:03 -0700

    adds the ability to filter client#topPage
    allows client#topPage to filter to response statuses, and other filters supported
    by the linkscape api.

--- spec/integration/linkscape/client_spec.rb
+++ spec/integration/linkscape/client_spec.rb
@@ -4,9 +4,12 @@ require 'linkscape/client'
 require 'yaml'
 require 'linkscape'
 
+require 'pp'
+require 'debugger'
+
 module Linkscape
   describe Client, :vcr, :vcr_recorded_time do
-
+    let(:client) { Client.new(credentials.fetch('accessID'), credentials.fetch('secretKey')) }
     let(:credentials) do
       YAML.load_file(Linkscape.root_directory + 'config/credentials.yml')
     end
@@ -20,10 +23,40 @@ module Linkscape
     end
 
     it "gets the latest mozscape index" do
-      client = Client.new(credentials.fetch('accessID'), credentials.fetch('secretKey'))
       data = client.index_name
       expect(data.index).to be > 0
       expect(data.sub_index).to be > 0
+    end
+
+    describe '#topPages' do
+      let(:uri)      { 'moz.com' }
+      let(:uri_type) { 'subdomain' }
+      let(:sort)     { :page_authority }
+      let(:schema_source_columns) do
+        [
+          :title,
+          :url,
+          :page_authority,
+          :pl_domains_linking,
+          :all_external_links,
+          :status,
+          :url_schema
+        ]
+      end
+
+      it 'supports filtering by response code' do
+        params = {
+            :cols => schema_source_columns,
+            :sort => sort,
+            :limit => 4,
+            :offset => 0,
+            :filter => 'status4xx'
+          }
+        response = client.topPages(uri, uri_type, params)
+        statuses = response.data.map { |d| d[:source][:status] }
+        expect(statuses.size).to be > 0
+        statuses.each { |s| expect(s).to be_between(400, 499) }
+      end
     end
   end
 end
